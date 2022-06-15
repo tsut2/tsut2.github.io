01bdd0a596d10044339b2b095cc39cee58b00fdf
t^2 <106585496+tsut2@users.noreply.github.com>
2022-06-15 15:54:08 +0900

    Add files via upload

--- todoapp/script.js
+++ todoapp/script.js
@@ -1,123 +1,268 @@
-document.oncontextmenu = () => { return false };
-const elemMenu = document.getElementById("menu");
+const elementMenu = document.getElementById("menu");
+let currentList = null;
+document.oncontextmenu=()=>{return false};
+window.onload=()=>{
+	loadLists();
 
-let tempListName = window.prompt("リスト名の入力");
-const listName = tempListName !== null && tempListName.length > 0 ? tempListName : "default";
+	// if (Notification.permission === "default"){
+	// 	Notification.requestPermission().then((result) => {
+	// 		console.log(result);
+	// 	});
+	// }
 
-window.onload = () => {
-	document.getElementById("header").textContent = listName;
-	
-	if (localStorage.getItem(listName) !== null) {
-		const tasks = JSON.parse(localStorage.getItem(listName));
-		for (const task of tasks) {
-			// fixme: 重複してる
-			let div = document.createElement("div");
-			div.id = task[0];
-			div.classList.add("selectable", "elem", "task", "data", task[1] ? "done" : null);
-			div.textContent = task[2];
-			div.addEventListener(
-				"contextmenu",
-				(event) => clickMenu(div, event)
-			);
-			document.getElementById("block_task").prepend(div);
+	setInterval(()=>{
+		for (const elem of document.getElementsByClassName("task")){
+			const now = Date.now();
+			if(elem.dataset.deadline !== "0" && !elem.classList.contains("expired") && parseInt(elem.dataset.deadline) < now){
+				elem.classList.add("expired");
+			}
 		}
-	}
 
-	for (const elem of document.getElementsByClassName("elem")){
-		elem.addEventListener("contextmenu", (event) => clickMenu(elem, event));
-	}
+		// if (window.Notification && Notification.permission === "granted"){
+		// 	let n = new Notification(
+		// 		"title", {body: "期限切れ"}
+		// 	);
+		// }
 
-	document.body.addEventListener("click", () => {
-		elemMenu.style.display = "none";
+		save();
+	},100);
+
+	document.addEventListener("contextmenu", (event)=>menu(event));
+	document.addEventListener("click", ()=>{
+		elementMenu.style.display = "none";
 	});
 }
-
-function genIdentifier(d = 16){
-	let charList = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
-		temp = "";
-	for (let i = 0; i < d; i++) {
+function genIdentifier(digit=16){
+	const charList = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_-"
+	let temp = "";
+	for (let i = 0; i < digit; i++) {
 		temp += charList.charAt(Math.floor(Math.random() * charList.length));
 	}
 	return temp;
 }
+function loadLists(){
+	const data = JSON.parse(localStorage.getItem("listNames"));
 
-function mkMenu(text, func){
-	let div = document.createElement("div");
-	div.textContent = text;
-	div.classList.add("selectable");
-	div.onclick = func;
-	elemMenu.appendChild(div);
-}
-
-function clickMenu(elem, mouseEvent){
-	while(elemMenu.firstChild){
-		elemMenu.removeChild(elemMenu.firstChild);
-	}
-
-	elemMenu.style.left    = mouseEvent.pageX + "px";
-	elemMenu.style.top     = mouseEvent.pageY + "px";
-	elemMenu.style.display = "block";
-
-	if (elem.classList.contains("task")){ // todo: 分割
-		if (elem.id !== "block_task"){
-			mkMenu("編集", () => {
-				const oldText = elem.textContent;
-				const newText = window.prompt("", oldText)
-				elem.textContent = newText !== null && newText.length > 0 ? newText : oldText;
-			});
-			mkMenu(elem.classList.contains("done") ? "未完了" : "完了", () => {
-				elem.classList.toggle("done")
-			});
-			mkMenu("消す", () => {
-				document.getElementById(elem.id).remove()
-			});
-			// mkMenu("deadline");
+	if (data !== null){
+		let firstListName = null;
+		for (const datum of data) {
+			if (firstListName === null || firstListName.length === 0){
+				firstListName = datum;
+			}
+			mkList(datum);
 		}
-		mkMenu("追加する", () => {
-			let div = document.createElement("div");
-			div.id = genIdentifier();
-			div.classList.add("selectable", "elem", "task", "data");
-			div.textContent = "new task";
-			div.addEventListener(
-				"contextmenu",
-				(event) => clickMenu(div, event)
-			);
-			document.getElementById("block_task").prepend(div);
-		});
-		mkMenu("完了済を消す", () => {
-			let count = 0, array = [];
-			for (const e of document.getElementsByClassName("done")) {
-				array[count++] = e;
-			}
-			if ( count > 0 && window.confirm("本当に消す?")) {
-				for (const e of array.reverse()) {
-					e.remove();
-				}
-			}
-		});
-		mkMenu("保存する", () => {
-			let data = [];
-			for (const e of document.getElementsByClassName("data")) {
-				data.push([
-					e.id,
-					e.classList.contains("done"),
-					e.textContent
-				]);
-			}
-			localStorage.setItem(listName, JSON.stringify(data));
-		});
-		mkMenu("他のリスト", () => {
-			window.location.reload();
-		});
-	}
-	mouseEvent.preventDefault();
-	mouseEvent.stopPropagation();
-}
+		return firstListName;
+	} else {
+		const data_l = ["説明"];
+		const data_t = [
+			["左にあるのはリストです", false, 0],
+			["クリックでリストを開きます", false, 0],
+			["右にあるのはタスクです", false, 0],
+			["クリックで完了済みに切り替えます", false, 0],
+			["完了済みになると色が変わります", true, 0],
+			["もう一度クリックすることで未完了に戻せます", false, 0],
+			["右クリックでメニューを開きます", false, 0],
+			["リストの追加, 編集などを行えます", false, 0]
+		];
 
-const test_setTasks = (c=32) => {
-	let data=[];
-	for (let i=0; i<c;i++){data.push([genIdentifier(),Math.random()>0.5,`testTask_${genIdentifier(8)}`])}
-	localStorage.setItem(listName, JSON.stringify(data));
-	window.alert(`リスト名:${listName}に${c}個\nリロードします`);
-	window.location.reload();
+		localStorage.setItem("listNames", JSON.stringify(data_l));
+		localStorage.setItem("L説明", JSON.stringify(data_t));
+
+		loadTasks(loadLists());
+
+		currentList="説明";
+		document.getElementsByClassName("list")[1].classList.add("open");
+	}
+}
+function loadTasks(key){
+	removeChildren("block_task");
+	currentList = key;
+
+	const data = JSON.parse(localStorage.getItem("L"+key));
+
+	if (data !== null) {
+		for (const datum of data) {
+			mkTask(datum[0], datum[1], datum[2]);
+		}
+	}
+}
+function openList(elem){
+	save();
+
+	for (const argument of document.getElementsByClassName("open")){
+		argument.classList.remove("open");
+	}
+
+	elem.classList.add("open");
+	loadTasks(elem.textContent);
+}
+function removeList(elem){
+	if (window.confirm(elem.textContent + "を消しますか?")) {
+		elem.remove();
+		if (elem.textContent === currentList) {
+			removeChildren("block_task");
+		}
+		localStorage.removeItem("L"+elem.textContent);
+	}
+}
+function mkList(name){
+	const div = document.createElement("div");
+	div.textContent = name;
+	div.id = "L"+genIdentifier();
+
+	div.classList.add("list", "selectable");
+
+	div.addEventListener("click", ()=>openList(div));
+
+	document.getElementById("block_list").appendChild(div);
+}
+function mkTask(text, isDone, deadline){
+	const div = document.createElement("div");
+	div.textContent = text;
+
+	div.id = "T"+genIdentifier();
+	div.classList.add("task", "selectable");
+	if (isDone){ div.classList.add("done")}
+	div.dataset.deadline = deadline;
+
+	div.addEventListener("click", ()=>{div.classList.toggle("done")});
+	// div.addEventListener("contextmenu", (e)=>taskMenu(e, div.id));
+
+	document.getElementById("block_task").appendChild(div);
+}
+function removeChildren(targetId){
+	const elem = document.getElementById(targetId);
+	while(elem.firstChild){
+		elem.removeChild(elem.firstChild);
+	}
+}
+function newList(){
+	const name = prompt("新規リスト名: 変更できません", "新規リスト");
+	let   lists = [];
+
+	for (const nameElement of document.getElementsByClassName("list")) {
+		lists.push(nameElement.textContent);
+	}
+
+	if (name === null || name.length === 0) {
+		alert("リスト名が空です");
+	} else if (lists.includes(name)) {
+		alert(name + " は存在しています");
+	} else {
+		mkList(name);
+	}
+}
+function removeTask(clickedElem){
+	clickedElem.remove();
+}
+function editTask(clickedElem){
+	const temp = window.prompt("", clickedElem.textContent);
+	clickedElem.textContent = temp !== null && temp.length > 0 ? temp : clickedElem.textContent
+}
+function setDeadlineTask(clickedElem){
+	const now = new Date();
+	const time = new Date(
+		parseInt(window.prompt("FullYear", now.getFullYear().toString())) % 9999,
+		parseInt(window.prompt("Month", (now.getMonth()+1).toString()))-1 % 12,
+		parseInt(window.prompt("Date", now.getDate().toString())) % 31,
+		parseInt(window.prompt("Hours", now.getHours().toString())) % 24,
+		parseInt(window.prompt("Minutes", now.getMinutes().toString())) % 60,
+		parseInt(window.prompt("Seconds", now.getSeconds().toString())) % 60
+	);
+
+	clickedElem.dataset.deadline = time.getTime() > Date.now() ? time.getTime().toString() : 0;
+	clickedElem.classList.remove("expired");
+
+	// let time = window.prompt(
+	// 	"時間を入力",
+	// 	date.toLocaleDateString() + "-" + date.toLocaleTimeString()
+	// );
+	// if (time !== null){
+	// 	clickedElem.dataset.deadline = time;
+	// 	alert(time);
+	// } else {
+	// 	alert("異常値です");
+	// }
+}
+function newTask(){
+	const temp = window.prompt("", "新規タスク");
+	mkTask(temp !== null && temp.length > 0 ? temp : "新規タスク", false, 0);
+}
+function removeCompletedTask(){
+	let count = 0, array = [];
+	for (const e of document.getElementsByClassName("done")) {
+		array[count++] = e;
+	}
+	if ( count > 0 && window.confirm("本当に消す?")) {
+		for (const e of array.reverse()) {
+			e.remove();
+		}
+	} else {
+		alert("完了しているものがありません");
+	}
+}
+function save(){
+	let lists = [], tasks = [];
+
+	if (document.getElementById("block_list").children.length !== 0) {
+		for (const list of document.getElementById("block_list").children) {
+			lists.push(list.textContent);
+			localStorage.setItem("listNames", JSON.stringify(lists));
+		}
+	}
+
+	if (document.getElementById("block_task").children.length !== 0) {
+		for (const task of document.getElementById("block_task").children) {
+			tasks.push([task.textContent, task.classList.contains("done"), task.dataset.deadline]);
+		}
+		localStorage.setItem("L"+currentList, JSON.stringify(tasks));
+	}
+}
+function showDeadlineTask(clickedElem){
+	let time = new Date(parseInt(clickedElem.dataset.deadline));
+	alert(time.toLocaleDateString()+"-"+time.toLocaleTimeString());
+}
+function menu(event){
+	const clickedElem = event.composedPath()[0];
+	const addMenu=(text, func)=>{
+		let div = document.createElement("div");
+		div.textContent = text;
+		div.classList.add("selectable");
+		div.addEventListener("click", func);
+		elementMenu.appendChild(div);
+	}
+
+	removeChildren(elementMenu.id);
+	elementMenu.style.left    = event.pageX+"px";
+	elementMenu.style.top     = event.pageY+"px";
+	elementMenu.style.display = "block";
+
+	if (clickedElem.classList.contains("list")){
+		if (clickedElem.id !== "block_list"){
+			addMenu("開く", ()=>openList(clickedElem));
+			if (document.getElementsByClassName("list").length > 2){
+				addMenu("削除", ()=>removeList(clickedElem));
+			}
+			// addMenu("リスト名変更", ()=>renameList(clickedElem));
+		}
+
+		addMenu("新規リスト", newList);
+	} else
+	if (clickedElem.classList.contains("task")){
+		if (clickedElem.id !== "block_task"){
+			addMenu(clickedElem.classList.contains("done") ? "未完了" : "完了", ()=>{
+				clickedElem.classList.toggle("done")
+			});
+			addMenu("期限", ()=>setDeadlineTask(clickedElem));
+			addMenu("期限の確認", ()=>showDeadlineTask(clickedElem));
+			addMenu("削除", ()=>removeTask(clickedElem));
+			addMenu("編集", ()=>editTask(clickedElem));
+		}
+		if (currentList !== null) {
+			addMenu("新規タスク", newTask);
+			addMenu("完了済みを削除", removeCompletedTask);
+		}
+	} else {elementMenu.style.display = "none"}
+
+	// addMenu("保存", ()=>save());
 }
---
+++ todoapp/test.js
@@ -0,0 +1,30 @@
+const testMkLS=()=>{
+	const lists = [
+		"List:foo",  "List:bar",
+		"List:bruh", "List:hoge",
+		"List:fuga", "List:piyo"
+	];
+
+	localStorage.setItem("listNames", JSON.stringify(lists));
+
+	for (const list of lists) {
+		const data = [];
+
+		for (let i=0; i++<10;) {
+			data.push([
+				Math.floor(10*Math.random()).toString(),
+				Math.random() < 0.5,
+				0
+			]);
+		}
+		localStorage.setItem(list, JSON.stringify(data));
+	}
+
+	alert("reload");
+	location.reload();
+}
+function clearAll(){
+	removeChildren("block_list");
+	removeChildren("block_task");
+	localStorage.clear()
+}
